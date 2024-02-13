6655a747a4d325e027a412036f9951a0a747c385
yuhaeun-la <exok1and1m@gmail.com>
2024-02-14 02:33:31 +0900

    [Feat] SettingView add

--- Projects/Feature/SearchFeature/Sources/View/AfterSearchView.swift
+++ Projects/Feature/SearchFeature/Sources/View/AfterSearchView.swift
@@ -11,58 +11,6 @@ import UIKit
 import Core
 import DesignSystem
 
-final class AfterSearchView: UITableViewHeaderFooterView {
+final class AfterSearchView: UIScrollView {
     
-    private let regionNameLabel: UILabel = {
-        let label = UILabel()
-        label.font =
-        DesignSystemFontFamily.NanumSquareNeoOTF.regular.font(size: 16)
-        label.textColor =
-        DesignSystemAsset.routeTimeColor.color
-        return label
-    }()
-    
-    private let busStopNameLabel: UILabel = {
-        let label = UILabel()
-        label.font =
-        DesignSystemFontFamily.NanumSquareNeoOTF.regular.font(size: 14)
-        label.textAlignment = .left
-        label.textColor = .black
-        
-        return label
-    }()
-    
-    private let numberDirectionLabel: UILabel = {
-        let label = UILabel()
-        label.font =
-        DesignSystemFontFamily.NanumSquareNeoOTF.regular.font(size: 11)
-        label.textAlignment = .left
-        label.textColor = DesignSystemAsset.gray5.color
-        
-        return label
-    }()
-    
-    override init(reuseIdentifier: String?) {
-        super.init(reuseIdentifier: reuseIdentifier)
-        configureUI()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func configureUI() {
-        [busStopNameLabel, numberDirectionLabel].forEach {
-            contentView.addSubview($0)
-            $0.translatesAutoresizingMaskIntoConstraints = false
-        }
-        
-        NSLayoutConstraint.activate([
-            busStopNameLabel.leadingAnchor
-            .constraint(equalTo: contentView.leadingAnchor,
-                       constant: 30),
-            numberDirectionLabel.leadingAnchor
-                .constraint(equalTo: busStopNameLabel.leadingAnchor)
-        ])
-    }
 }
---
+++ Projects/Feature/SearchFeature/Sources/ViewController/AfterSearchViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AfterSearchViewController.swift
+//  SearchFeature
+//
+//  Created by 유하은 on 2024/02/13.
+//  Copyright © 2024 Pepsi-Club. All rights reserved.
+//
+
+import Foundation
