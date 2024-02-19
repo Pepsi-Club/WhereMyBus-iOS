1fe53bc3b204659a3a4d5e9f218de1af23d7f238
Geonseob Kim <gnksbm@gmail.com>
2024-02-19 13:15:12 +0900

    [Add] RegularAlarmUseCase 추가

---
+++ Projects/Domain/Sources/UseCase/DefaultRegularAlarmUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  DefaultRegularAlarmUseCase.swift
+//  Domain
+//
+//  Created by gnksbm on 2/16/24.
+//  Copyright © 2024 Pepsi-Club. All rights reserved.
+//
+
+import Foundation
+
+public final class DefaultRegularAlarmUseCase: RegularAlarmUseCase {
+    
+}
---
+++ Projects/Domain/Sources/UseCase/Protocol/RegularAlarmUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  RegularAlarmUseCase.swift
+//  Domain
+//
+//  Created by gnksbm on 2/14/24.
+//  Copyright © 2024 Pepsi-Club. All rights reserved.
+//
+
+import Foundation
+
+public protocol RegularAlarmUseCase {
+    
+}
