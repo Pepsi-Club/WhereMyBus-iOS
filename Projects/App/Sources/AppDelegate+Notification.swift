a7c77853f86679b430282364456e02f9289d6de9
Geonseob Kim <gnksbm@gmail.com>
2024-02-28 21:30:19 +0900

    [Feat] AppDelegate Notification 설정 추가

---
+++ Projects/App/Sources/AppDelegate+Notification.swift
@@ -0,0 +1,40 @@
+//
+//  AppDelegate+Notification.swift
+//  App
+//
+//  Created by gnksbm on 2/27/24.
+//  Copyright © 2024 Pepsi-Club. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func configureNotification() {
+        UNUserNotificationCenter.current().delegate = self
+    }
+    
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (
+            UNNotificationPresentationOptions
+        ) -> Void
+    ) {
+        completionHandler([.banner, .badge, .sound])
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+        print(deviceToken.map { String(format: "%02.2hhx", $0) }.joined())
+    }
+    
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+}
