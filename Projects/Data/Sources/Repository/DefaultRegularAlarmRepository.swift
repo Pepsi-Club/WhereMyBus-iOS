0c570060e9c1280101ff162aa00bb164c7c93f7d
Geonseob Kim <gnksbm@gmail.com>
2024-02-28 21:10:58 +0900

    [Add] RegularAlarmRepository 추가

---
+++ Projects/Data/Sources/Repository/DefaultRegularAlarmRepository.swift
@@ -0,0 +1,21 @@
+//
+//  DefaultRegularAlarmRepository.swift
+//  Data
+//
+//  Created by gnksbm on 2/27/24.
+//  Copyright © 2024 Pepsi-Club. All rights reserved.
+//
+
+import Foundation
+
+import Domain
+
+public final class DefaultRegularAlarmRepository: RegularAlarmRepository {
+    public init() {
+        
+    }
+    
+    public func addNewAlarm() throws {
+        
+    }
+}
---
+++ Projects/Domain/Sources/RepositoryInterface/RegularAlarmRepository.swift
@@ -0,0 +1,13 @@
+//
+//  RegularAlarmRepository.swift
+//  Domain
+//
+//  Created by gnksbm on 2/27/24.
+//  Copyright © 2024 Pepsi-Club. All rights reserved.
+//
+
+import Foundation
+
+public protocol RegularAlarmRepository {
+    func addNewAlarm() throws
+}
