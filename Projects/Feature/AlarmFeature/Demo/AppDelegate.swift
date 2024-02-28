359132a6d501c84755e04c3161bc3b4597469168
Geonseob Kim <gnksbm@gmail.com>
2024-02-28 21:29:29 +0900

    [Add] AlarmFeature DemoApp Mock객체 구현

--- Projects/Feature/AlarmFeature/Demo/AppDelegate.swift
+++ Projects/Feature/AlarmFeature/Demo/AppDelegate.swift
@@ -1,11 +1,16 @@
 import UIKit
 
+import Core
+import Domain
 import DesignSystem
+
+import RxSwift
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         Appearance.setupAppearance()
+        register()
         return true
     }
 
@@ -16,5 +21,45 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+    
+    func register() {
+        DIContainer.register(
+            type: RegularAlarmUseCase.self,
+            DefaultRegularAlarmUseCase(
+                regularAlarmRepository: MockRegularAlarmRepository(),
+                localNotificationService: MockLocalNotificationService()
+            )
+        )
+    }
+}
+
+final class MockRegularAlarmRepository: RegularAlarmRepository {
+    func addNewAlarm() throws {
+        
+    }
+}
+
+final class MockLocalNotificationService: LocalNotificationService {
+    var authState = BehaviorSubject<UNAuthorizationStatus>(value: .denied)
+    
+    func authorize() {
+        
+    }
+    
+    func fetchRegularAlarm() {
+        
+    }
+    
+    func registNewRegularAlarm(response: Domain.RegularAlarmResponse) throws {
+        print(response)
+    }
+    
+    func editRegularAlarm() throws {
+        
+    }
+    
+    func deleteRegularAlarm() throws {
+        
     }
 }
