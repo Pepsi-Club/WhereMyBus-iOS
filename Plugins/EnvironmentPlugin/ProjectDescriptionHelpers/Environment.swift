b2472f770c6dfd66999de0cf2241e26014642422
Geonseob Kim <109283556+gnksbm@users.noreply.github.com>
2024-03-28 03:02:40 +0900

    [Chore] 버전 수정

--- Plugins/EnvironmentPlugin/ProjectDescriptionHelpers/Environment.swift
+++ Plugins/EnvironmentPlugin/ProjectDescriptionHelpers/Environment.swift
@@ -14,7 +14,7 @@ public extension String {
     static let organizationName = "Pepsi-Club"
     static let teamId = "T4W7695R5C"
     /// 앱스토어에 게시할 때마다 증가해줘야 하는 버전
-    static let marketingVersion: Self = "1.0.2"
+    static let marketingVersion: Self = "1.0.3"
     /// 개발자가 내부적으로 확인하기 위한 용도 (날짜를 사용하기도 함 - 2023.12.8.1 )
     static var buildVersion: Self {
         "1.0"
