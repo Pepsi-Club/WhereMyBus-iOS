dbd32d1eec8dc05c89daee40de82ef395dd0e7ca
Geonseob Kim <109283556+gnksbm@users.noreply.github.com>
2024-04-06 16:35:51 +0900

    [Fix] 정규알람 모델 수정 Response -> DTO

--- Projects/Data/Sources/DTO/RemoveRegularAlarmDTO.swift
+++ Projects/Data/Sources/DTO/RemoveRegularAlarmDTO.swift
@@ -1,6 +1,6 @@
 //
-//  RemoveRegularAlarmResponse.swift
-//  Domain
+//  RemoveRegularAlarmDTO.swift
+//  Data
 //
 //  Created by gnksbm on 4/6/24.
 //  Copyright © 2024 Pepsi-Club. All rights reserved.
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public struct RemoveRegularAlarmResponse: Decodable {
+struct RemoveRegularAlarmDTO: Decodable {
     /// 메시지 (ex. 성공, DB Error 등)
     public let message: String
     
