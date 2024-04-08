bddaf3781889d97ca47e21b8009704379c71d022
Geonseob Kim <109283556+gnksbm@users.noreply.github.com>
2024-04-08 14:47:32 +0900

    [Fix] #205 DeeplinkHandler 호출 및 로직 연결

--- Projects/App/Sources/Coordinator/AppCoordinator.swift
+++ Projects/App/Sources/Coordinator/AppCoordinator.swift
@@ -10,12 +10,14 @@ import UIKit
 
 import FeatureDependency
 import MainFeature
+import BusStopFeature
 
 final class AppCoordinator: Coordinator {
     var parent: Coordinator?
     var childs: [Coordinator] = []
     var navigationController: UINavigationController
     public var coordinatorType: CoordinatorType = .app
+    private let coordinatorProvider = DefaultCoordinatorProvider()
     
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
@@ -24,9 +26,21 @@ final class AppCoordinator: Coordinator {
     func start() {
         let tabBarCoordinator = TabBarCoordinator(
             navigationController: navigationController, 
-            coordinatorProvider: DefaultCoordinatorProvider()
+            coordinatorProvider: coordinatorProvider
         )
         childs.append(tabBarCoordinator)
         tabBarCoordinator.start()
+    }
+    
+    func startBusStopFlow(busStopId: String) {
+        let busStopCoordinator = DefaultBusStopCoordinator(
+            parent: self,
+            navigationController: navigationController,
+            busStopId: busStopId,
+            coordinatorProvider: coordinatorProvider,
+            flow: .fromHome
+        )
+        childs.append(busStopCoordinator)
+        busStopCoordinator.start()
     }
 }
