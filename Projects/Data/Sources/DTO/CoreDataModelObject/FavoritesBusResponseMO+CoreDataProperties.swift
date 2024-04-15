84080503790ec5b63a2021c06d39ec524ead8dd4
Geonseob Kim <109283556+gnksbm@users.noreply.github.com>
2024-04-16 00:30:53 +0900

    [Add] #234 FavoritesBusResponseMO 추가

---
+++ Projects/Data/Sources/DTO/CoreDataModelObject/FavoritesBusResponseMO+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  FavoritesBusResponseMO+CoreDataProperties.swift
+//  
+//
+//  Created by gnksbm on 4/16/24.
+//
+//
+
+import Foundation
+import CoreData
+
+extension FavoritesBusResponseMO {
+    @nonobjc public class func fetchRequest(
+    ) -> NSFetchRequest<FavoritesBusResponseMO> {
+        return NSFetchRequest<FavoritesBusResponseMO>(
+            entityName: "FavoritesBusResponseMO"
+        )
+    }
+
+    @NSManaged public var busStopId: String?
+    @NSManaged public var busStopName: String?
+    @NSManaged public var busId: String?
+    @NSManaged public var busName: String?
+    @NSManaged public var adirection: String?
+}
