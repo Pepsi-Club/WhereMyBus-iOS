8def008d1f007fc409b4efe75c8fa0b8fc10bd9d
yuhaeun-la <exok1and1m@gmail.com>
2024-04-16 17:51:18 +0900

    [add] 2x2 ArrivalInfoView

---
+++ Projects/App/Widget/ArrivalInfo/View/ArrivalInfoSmall2View.swift
@@ -0,0 +1,19 @@
+//
+//  ArrivalInfoSmall2View.swift
+//  AppUITests
+//
+//  Created by 유하은 on 4/16/24.
+//  Copyright © 2024 Pepsi-Club. All rights reserved.
+//
+
+import SwiftUI
+
+struct ArrivalInfoSmall2View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArrivalInfoSmall2View()
+}
