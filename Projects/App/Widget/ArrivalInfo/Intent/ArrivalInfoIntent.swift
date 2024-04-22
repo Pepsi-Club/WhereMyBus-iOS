706346316735b5d9ae227c3702cd969f6ab941f6
yuhaeun <65907001+yuhaeun-la@users.noreply.github.com>
2024-04-22 15:31:59 +0900

    Update ArrivalInfoIntent.swift

--- Projects/App/Widget/ArrivalInfo/Intent/ArrivalInfoIntent.swift
+++ Projects/App/Widget/ArrivalInfo/Intent/ArrivalInfoIntent.swift
@@ -15,10 +15,7 @@ import Domain
 struct ArrivalInfoIntent: WidgetConfigurationIntent {
     static var title: LocalizedStringResource = "Configuration"
 
-<<<<<<< HEAD
     @Parameter(title: "정류장, 버스", optionsProvider: BusStopOptionProvider())
-=======
     @Parameter(title: "정류장, 버스", optionsProvider: FavoritesOptionProvider())
->>>>>>> dev
     var busStop: String
 }
