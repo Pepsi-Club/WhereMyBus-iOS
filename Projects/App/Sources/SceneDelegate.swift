f5dd3c8833dc7490c82e16c0ba26ff3f2429ff09
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2024-05-26 14:50:21 +0900

    [Feat] #311 앱스토어 버전과 프로젝트 버전 비교해서 업데이트 alert 주는 함수

--- Projects/App/Sources/SceneDelegate.swift
+++ Projects/App/Sources/SceneDelegate.swift
@@ -20,7 +20,7 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var deeplinkHandler: DeeplinkHandler?
 
     func scene(
-        _ scene: UIScene, 
+        _ scene: UIScene,
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
@@ -37,6 +37,7 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         )
         appCoordinator?.start()
         window?.makeKeyAndVisible()
+        self.checkAndUpdateIfNeeded()
         deeplinkHandler = .init(appCoordinator: appCoordinator)
         if let url = connectionOptions.urlContexts.first?.url {
             deeplinkHandler?.handleUrl(url: url)
@@ -52,7 +53,9 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillResignActive(_ scene: UIScene) {
     }
 
+    /// 앱이 Foreground로 전환될때 실행될 함수
     func sceneWillEnterForeground(_ scene: UIScene) {
+        self.checkAndUpdateIfNeeded()
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
@@ -65,6 +68,49 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         if let url = URLContexts.first?.url {
             deeplinkHandler?.handleUrl(url: url)
         }
+    }
+    
+    private func checkAndUpdateIfNeeded() {
+        AppStoreCheck.latestVersion { marketingVersion in
+            DispatchQueue.main.async {
+                guard let marketingVersion else { return }
+                
+                /// 현재 기기 버전
+                let currentProjectVersion = String.getCurrentVersion()
+                
+                /// .을 기준으로 나눔
+                let splitMarketingVersion = marketingVersion.split(separator: ".").map { $0 }
+                let splitCurrentProjectVersion = currentProjectVersion.split(separator: ".").map { $0 }
+                
+                if splitCurrentProjectVersion.count > 0 && splitMarketingVersion.count > 0 {
+                    // Major 버전만을 비교
+                    if splitCurrentProjectVersion[0] < splitMarketingVersion[0] {
+                        self.showUpdateAlert(version: marketingVersion)
+                    } else {
+                        print("현재 최신 버전입니다.")
+                    }
+                    
+                }
+            }
+        }
+    }
+    
+    private func showUpdateAlert(version: String) {
+        let alert = UIAlertController(
+            title: "업데이트 알림",
+            message: "더 나은 서비스를 위해 업데이트 되었어요 ! 업데이트 해주세요.",
+            preferredStyle: .alert
+        )
+        
+        let alertAction = UIAlertAction(
+            title: "업데이트",
+            style: .default) { _ in
+                AppStoreCheck.openAppStore()
+            }
+        
+        alert.addAction(alertAction)
+        window?.rootViewController?.present(alert, animated: true)
+        
     }
 }
 
