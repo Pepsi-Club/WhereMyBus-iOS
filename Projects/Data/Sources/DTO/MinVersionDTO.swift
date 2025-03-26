4f287873f1b5bb60d0c33ed2ec68333d9c4dac41
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2025-03-26 21:20:13 +0900

    [Add] #311 최소버전에 대한 Endpoint 및 DTO 모델

---
+++ Projects/Data/Sources/DTO/MinVersionDTO.swift
@@ -0,0 +1,35 @@
+//
+//  MinVersionDTO.swift
+//  Data
+//
+//  Created by Jisoo Ham on 3/26/25.
+//  Copyright © 2025 Pepsi-Club. All rights reserved.
+//
+
+import Foundation
+
+import Domain
+
+public struct MinVersionDTO: Decodable {
+    let version: String
+    
+    enum CodingKeys: String, CodingKey {
+        case version = "ver"
+    }
+}
+
+extension MinVersionDTO {
+    var toDomain: AppVersionInfoResponse? {
+        let versionComponents = version.split(separator: ".")
+            .compactMap { Int($0) }
+        
+        guard versionComponents.count == 3
+        else { return nil }
+        
+        return AppVersionInfoResponse(
+            major: versionComponents[0],
+            minor: versionComponents[1],
+            patch: versionComponents[2]
+        )
+    }
+}
