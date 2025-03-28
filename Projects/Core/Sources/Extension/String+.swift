c0dcc2e45efa713df0d9aa74e62be7e8fa4e918a
Jisoo Ham <133845468+isakatty@users.noreply.github.com>
2025-03-28 17:14:40 +0900

    Fix/#322 도메인 주소 및 적용 방식 변경 (#323)
    * [Add] api url 추가 및 적용 방식 변경 #322
    
    * [Fix] Lint에 맞춰 컨벤션 적용 #322
    
    * [Refactor] #322 변경된 도메인 url 적용

--- Projects/Core/Sources/Extension/String+.swift
+++ Projects/Core/Sources/Extension/String+.swift
@@ -34,13 +34,23 @@ public extension String {
         return serverKey
     }
     
+    /// domain url
+    static var domainURL: Self {
+        guard let any = Bundle.main.object(
+            forInfoDictionaryKey: "DOMAIN_URL"
+        ),
+              let domain = any as? String
+        else { return "" }
+        return domain
+    }
+    
     /// 프로젝트 버전
-    static func getCurrentVersion() -> [Int] {
+    static func getCurrentVersion() -> Self {
         guard let dictionary = Bundle.main.infoDictionary,
               let version = dictionary["CFBundleShortVersionString"] as? String
-        else { return [1, 0, 0] }
+        else { return "1.0.0" }
         
-        return version.split(separator: ".").compactMap { Int($0) }
+        return version
     }
     
     static func getDeviceIdentifier() -> String {
